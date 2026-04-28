6885f1e572c1161d8520f225b93fba4f197f2089
Ben Callaghan <benj.callaghan@gmail.com>
2026-04-28 13:32:24 -0600

    Added public constants

--- YubiHsmSharp/yubihsm.cs
+++ YubiHsmSharp/yubihsm.cs
@@ -47,5 +47,163 @@
 /// <seealso>yubihsm.h</seealso> 
 public static class yubihsm
 {
+    /// <summary>
+    /// Length of context array for authentication
+    /// </summary>
+    public const int YH_CONTEXT_LEN = 16;
 
+    /// <summary>
+    /// Length of host challenge for authentication
+    /// </summary>
+    public const int YH_HOST_CHAL_LEN = 8;
+
+#if !FUZZING
+    public const int YH_MSG_BUF_SIZE = 3136;
+#else
+    // In fuzzing builds make the data buffers smaller
+    public const int YH_MSG_BUF_SIZE = 100;
+#endif
+
+    /// <summary>
+    /// Length of authentication keys
+    /// </summary>
+    public const int YH_KEY_LEN = 16;
+
+    /// <summary>
+    /// Device vendor ID
+    /// </summary>
+    public const int YH_VID = 0x1050;
+
+    /// <summary>
+    /// Device product ID
+    /// </summary>
+    public const int YH_PID = 0x0030;
+
+    /// <summary>
+    /// Response flag for commands
+    /// </summary>
+    public const int YH_CMD_RESP_FLAG = 0x80;
+
+    /// <summary>
+    /// Max items the device may hold
+    /// </summary>
+    public const int YH_MAX_ITEMS_COUNT = 256;
+
+    /// <summary>
+    /// Max sessions the device may hold.
+    /// </summary>
+    public const int YH_MAX_SESSIONS = 16;
+
+    /// <summary>
+    /// Default encryption key
+    /// </summary>
+    public static readonly byte[] YH_DEFAULT_ENC_KEY =
+        [0x09, 0x0b, 0x47, 0xdb, 0xed, 0x59, 0x56, 0x54, 0x90, 0x1d, 0xee, 0x1c, 0xc6, 0x55, 0xe4, 0x20];
+
+    /// <summary>
+    /// Default MAC key
+    /// </summary>
+    public static readonly byte[] YH_DEFAULT_MAC_KEY =
+        [0x59, 0x2f, 0xd4, 0x83, 0xf7, 0x59, 0xe2, 0x99, 0x09, 0xa0, 0x4c, 0x45, 0x05, 0xd2, 0xce, 0x0a];
+
+    /// <summary>
+    /// Default authentication key password
+    /// </summary>
+    public const string YH_DEFAULT_PASSWORD = "password";
+
+    /// <summary>
+    /// Salt to be used for PBKDF2 key derivation
+    /// </summary>
+    public const string YH_DEFAULT_SALT = "Yubico";
+
+    /// <summary>
+    /// Number of iterations for PBKDF2 key derivation
+    /// </summary>
+    public const int YH_DEFAULT_ITERS = 10000;
+
+    /// <summary>
+    /// Length of capabilities array
+    /// </summary>
+    public const int YH_CAPABILITIES_LEN = 8;
+
+    /// <summary>
+    /// Max log entries the device may hold
+    /// </summary>
+    public const int YH_MAX_LOG_ENTRIES = 64;
+
+    /// <summary>
+    /// Max length of object labels
+    /// </summary>
+    public const int YH_OBJ_LABEL_LEN = 40;
+
+    /// <summary>
+    /// Max number of domains
+    /// </summary>
+    public const int YH_MAX_DOMAINS = 16;
+
+    /// <summary>
+    /// Size that the log digest is truncated to
+    /// </summary>
+    public const int YH_LOG_DIGEST_SIZE = 16;
+
+    /// <summary>
+    /// URL scheme used for direct USB access
+    /// </summary>
+    public const string YH_USB_URL_SCHEME = "yhusb://";
+
+    /// <summary>
+    /// URL scheme used for fuzzing builds
+    /// </summary>
+    public const string YH_FUZZ_URL_SCHEME = "yhfuzz://";
+
+    /// <summary>
+    /// Debug levels
+    /// </summary>
+    [Flags]
+    public enum yh_verbosity
+    {
+        /// <summary>
+        /// Debug level quiet. No messages printed out
+        /// </summary>
+        YH_VERB_QUIET = 0x00,
+
+        /// <summary>
+        /// Debug level intermediate. Intermediate results printed out
+        /// </summary>
+        YH_VERB_INTERMEDIATE = 0x01,
+
+        /// <summary>
+        /// Debug level crypto. Crypto results printed out
+        /// </summary>
+        YH_VERB_CRYPTO = 0x02,
+
+        /// <summary>
+        /// Debug level raw. Raw messages printed out
+        /// </summary>
+        YH_VERB_RAW = 0x04,
+
+        /// <summary>
+        /// Debug level info. General information messages printed out
+        /// </summary>
+        YH_VERB_INFO = 0x08,
+
+        /// <summary>
+        /// Debug level error. Error messages printed out
+        /// </summary>
+        YH_VERB_ERR = 0x10,
+
+        /// <summary>
+        /// Debug level all. All previous options enabled
+        /// </summary>
+        YH_VERB_ALL = 0xff,
+    }
+
+    /// <summary>
+    /// This is the overhead when doing aes-ccm wrapping: 1 byte identifier,
+    /// 13 bytes nonce and 16 bytes mac
+    /// </summary>
+    public const int YH_CCM_WRAP_OVERHEAD = 1 + 13 + 16;
+
+    public const int YH_EC_P256_PRIVKEY_LEN = 32;
+    public const int YH_EC_256_PUBKEY_LEN = 65;
 }
