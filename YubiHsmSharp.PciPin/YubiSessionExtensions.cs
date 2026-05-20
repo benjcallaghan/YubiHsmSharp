0908e95563c20676a7fd823f855f95ab442f5c11
Ben Callaghan <benj.callaghan@gmail.com>
2026-05-20 14:31:15 -0600

    added a method to import a full key block

--- YubiHsmSharp.PciPin/YubiSessionExtensions.cs
+++ YubiHsmSharp.PciPin/YubiSessionExtensions.cs
@@ -17,11 +17,12 @@ public static class YubiSessionExtensions
         /// The authentication key used to open the session requires the following delegated-capabilities:
         /// encrypt-ecb, decrypt-ecb.
         /// </remarks>
-        /// <param name="utf8Label">The label of the opaque object, UTF-8 encoded and null-terminated.</param>
-        /// <param name="domains">The domains where the opaque object will be operating within.</param>
+        /// <param name="utf8Label">The label of the symmetric key, UTF-8 encoded and null-terminated.</param>
+        /// <param name="domains">The domains where the symmetric key will be operating within.</param>
         /// <param name="zoneMasterKey">The key to import.</param>
         /// <param name="keyId">The ID of the ZMK. 0 if the ID should be assigned by the device.</param>
         /// <returns>The ID of the imported key.</returns>
+        /// <exception cref="ArgumentException">Thrown if the ZMK is not a valid AES-128, AES-192, or AES-256 key.
         public ushort ImportZoneMasterKey(ReadOnlySpan<byte> utf8Label, Domains domains, ReadOnlySpan<byte> zoneMasterKey, ushort keyId = 0)
         {
             Capabilities capabilities = Capabilities.From("encrypt-ecb,decrypt-ecb"u8);
@@ -44,6 +45,47 @@ public static class YubiSessionExtensions
         {
             ObjectDescriptor descriptor = session.GetObject(keyId, ObjectType.SymmetricKey);
             return new YubiSymmetricKeyParameter(keyId, descriptor.Length);
+        }
+
+        /// <summary>
+        /// Imports an AES Symmetric Key from a <see cref="TR31KeyBlock"/>.
+        /// </summary>
+        /// <remarks>
+        /// The authentication key used to open the session requires the following capabilities:
+        /// put-symmetric-key, encrypt-ecb.
+        /// The authentication key used to open the session requires the following delegated capabilities:
+        /// encrypt-ecb, decrypt-ecb.
+        /// </remarks>
+        /// <param name="keyBlock">The key block to decrypt and import.</param>
+        /// <param name="zoneMasterKeyId">The ID of the symmetric Zone Master Key (ZMK).</param>
+        /// <param name="utf8Label">The label of the symmetrtic key, UTF-8 encoded and null-terminated.</param>
+        /// <param name="domains">The domains where the symmetric key will be operating within.</param>
+        /// <param name="keyId">The ID of the imported key. 0 if the ID should be assigned by the device.</param>
+        /// <returns>The ID of the imported key.</returns>
+        /// <exception cref="ArgumentException">Thrown if the imported key is not a valid AES-128, AES-192, or AES-256 key.
+        public ushort ImportKeyBlock(TR31KeyBlock keyBlock, ushort zoneMasterKeyId, ReadOnlySpan<byte> utf8Label,
+            Domains domains, ushort keyId = 0)
+        {
+            if (keyBlock.Algorithm != KeyAlgorithm.AdvancedEncryptionStandard)
+            {
+                throw new ArgumentException("Only AES keys are supported.", nameof(keyBlock));
+            }
+
+            YubiSymmetricKeyParameter keyParameter = session.GetSymmetricKeyParameter(zoneMasterKeyId);
+
+            Span<byte> clearKey = stackalloc byte[keyParameter.KeyLength];
+            int written = keyBlock.Decrypt(new YubiAesBlockCipher(session), keyParameter, clearKey);
+            clearKey = clearKey[..written];
+
+            Capabilities capabilities = Capabilities.From("encrypt-ecb,decrypt-ecb"u8);
+            Algorithm algorithm = clearKey.Length switch
+            {
+                16 => Algorithm.Aes128,
+                24 => Algorithm.Aes192,
+                32 => Algorithm.Aes256,
+                _ => throw new ArgumentException("The provided key has an invalid length.", nameof(keyBlock)),
+            };
+            return session.ImportAesKey(utf8Label, domains, capabilities, algorithm, clearKey, keyId);
         }
     }
 }
