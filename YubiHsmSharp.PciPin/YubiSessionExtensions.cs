95952ddf9d8cde170e6f26f5bc69c650ffea476b
Ben Callaghan <benj.callaghan@gmail.com>
2026-05-20 14:54:08 -0600

    added key block metadata to the stored symmetric key

--- YubiHsmSharp.PciPin/YubiSessionExtensions.cs
+++ YubiHsmSharp.PciPin/YubiSessionExtensions.cs
@@ -22,7 +22,7 @@ public static class YubiSessionExtensions
         /// <param name="zoneMasterKey">The key to import.</param>
         /// <param name="keyId">The ID of the ZMK. 0 if the ID should be assigned by the device.</param>
         /// <returns>The ID of the imported key.</returns>
-        /// <exception cref="ArgumentException">Thrown if the ZMK is not a valid AES-128, AES-192, or AES-256 key.
+        /// <exception cref="ArgumentException">Thrown if the ZMK is not a valid AES-128, AES-192, or AES-256 key.</exception>
         public ushort ImportZoneMasterKey(ReadOnlySpan<byte> utf8Label, Domains domains, ReadOnlySpan<byte> zoneMasterKey, ushort keyId = 0)
         {
             Capabilities capabilities = Capabilities.From("encrypt-ecb,decrypt-ecb"u8);
@@ -33,7 +33,7 @@ public static class YubiSessionExtensions
                 32 => Algorithm.Aes256,
                 _ => throw new ArgumentException("The provided key has an invalid length.", nameof(zoneMasterKey)),
             };
-            return session.ImportAesKey(utf8Label, domains, capabilities, algorithm, zoneMasterKey, keyId);
+            return session.ImportAesKey(utf8Label, domains, in capabilities, algorithm, zoneMasterKey, keyId);
         }
 
         /// <summary>
@@ -54,7 +54,8 @@ public static class YubiSessionExtensions
         /// The authentication key used to open the session requires the following capabilities:
         /// put-symmetric-key, encrypt-ecb.
         /// The authentication key used to open the session requires the following delegated capabilities:
-        /// encrypt-ecb, decrypt-ecb.
+        /// encrypt-ecb, decrypt-ecb, exportable-under-wrap.
+        /// The key block's Mode of Use and Exportability are retained as YubiHSM 2 capabilities.
         /// </remarks>
         /// <param name="keyBlock">The key block to decrypt and import.</param>
         /// <param name="zoneMasterKeyId">The ID of the symmetric Zone Master Key (ZMK).</param>
@@ -62,8 +63,9 @@ public static class YubiSessionExtensions
         /// <param name="domains">The domains where the symmetric key will be operating within.</param>
         /// <param name="keyId">The ID of the imported key. 0 if the ID should be assigned by the device.</param>
         /// <returns>The ID of the imported key.</returns>
-        /// <exception cref="ArgumentException">Thrown if the imported key is not a valid AES-128, AES-192, or AES-256 key.
-        public ushort ImportKeyBlock(TR31KeyBlock keyBlock, ushort zoneMasterKeyId, ReadOnlySpan<byte> utf8Label,
+        /// <exception cref="ArgumentException">Thrown if the imported key is not a valid AES-128, AES-192, or AES-256 key,
+        /// or if the key block has an unsupported Mode of Use, or if the key block has an unsupported Exporatability.</exception>
+        public ushort ImportAesKey(TR31KeyBlock keyBlock, ushort zoneMasterKeyId, ReadOnlySpan<byte> utf8Label,
             Domains domains, ushort keyId = 0)
         {
             if (keyBlock.Algorithm != KeyAlgorithm.AdvancedEncryptionStandard)
@@ -71,13 +73,30 @@ public static class YubiSessionExtensions
                 throw new ArgumentException("Only AES keys are supported.", nameof(keyBlock));
             }
 
+            Capabilities modeOfUse = Capabilities.From(keyBlock.ModeOfUse switch
+            {
+                KeyUse.NoRestriction => "encrypt-ecb,decrypt-ecb"u8,
+                KeyUse.EncryptDecrypt => "encrypt-ecb,decrypt-ecb"u8,
+                KeyUse.Encrypt => "encrypt-ecb"u8,
+                KeyUse.Decrypt => "decrypt-ecb"u8,
+                _ => throw new ArgumentException($"The key use '{keyBlock.ModeOfUse}' is not supported.", nameof(keyBlock))
+            });
+            Capabilities exportability = keyBlock.Exportability switch
+            {
+                // Assuming the caller has a wrap key that is "extra sensitive" compatible.
+                KeyExportability.ExtraSensitive => Capabilities.From("exportable-under-wrap"u8),
+                KeyExportability.NonExportable => new Capabilities(),
+                KeyExportability.Sensitive => Capabilities.From("exportable-under-wrap"u8),
+                _ => throw new ArgumentException($"The exportablity '{keyBlock.Exportability}' is not supported.", nameof(keyBlock));
+            };
+            Capabilities capabilities = modeOfUse.Merge(in exportability);
+
             YubiSymmetricKeyParameter keyParameter = session.GetSymmetricKeyParameter(zoneMasterKeyId);
 
             Span<byte> clearKey = stackalloc byte[keyParameter.KeyLength];
             int written = keyBlock.Decrypt(new YubiAesBlockCipher(session), keyParameter, clearKey);
             clearKey = clearKey[..written];
 
-            Capabilities capabilities = Capabilities.From("encrypt-ecb,decrypt-ecb"u8);
             Algorithm algorithm = clearKey.Length switch
             {
                 16 => Algorithm.Aes128,
@@ -85,7 +104,7 @@ public static class YubiSessionExtensions
                 32 => Algorithm.Aes256,
                 _ => throw new ArgumentException("The provided key has an invalid length.", nameof(keyBlock)),
             };
-            return session.ImportAesKey(utf8Label, domains, capabilities, algorithm, clearKey, keyId);
+            return session.ImportAesKey(utf8Label, domains, in capabilities, algorithm, clearKey, keyId);
         }
     }
 }
