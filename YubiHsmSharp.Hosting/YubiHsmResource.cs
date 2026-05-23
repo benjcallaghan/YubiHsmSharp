0d8f6385ffc79159ac4f4e0abd98892fd4c158c5
Ben Callaghan <benj.callaghan@gmail.com>
2026-05-22 18:54:16 -0600

    added resource definitions for Aspire hosting

--- YubiHsmSharp.Hosting/YubiHsmResource.cs
+++ YubiHsmSharp.Hosting/YubiHsmResource.cs
@@ -1,14 +1,88 @@
 namespace Aspire.Hosting.ApplicationModel;
 
-public sealed class YubiHsmResource([ResourceName] string name) : IResourceWithConnectionString, IResourceWithEndpoints
+public sealed class YubiHsmResource([ResourceName] string name) : Resource(name),
+    IResourceWithEndpoints, IResourceWithoutLifetime, IResourceWithConnectionString
 {
-    private EndpointReference? yubiReference;
-    public EndpointReference HttpEndpoint => this.yubiReference ??= new(this, "http");
+    internal const string YubiHsmEndpointName = "yubihsm";
 
-    public ReferenceExpression ConnectionStringExpression =>
-        ReferenceExpression.Create($"http://{this.HttpEndpoint.Property(EndpointProperty.HostAndPort)}");
+    private EndpointReference? yubiHsmEndpoint;
+    public EndpointReference YubiHsmEndpoint =>
+        this.yubiHsmEndpoint ??= new(this, YubiHsmEndpointName);
 
-    public string Name => name;
+    public ReferenceExpression Url =>
+        ReferenceExpression.Create($"{this.YubiHsmEndpoint.Property(EndpointProperty.Url)}");
 
-    public ResourceAnnotationCollection Annotations => throw new NotImplementedException();
+    public ParameterResource? AuthKeyId { get; private set; }
+
+    public ParameterResource? Password { get; private set; }
+
+    public ParameterResource? EncryptionKey { get; private set; }
+
+    public ParameterResource? MacKey { get; private set; }
+
+    public string? EncryptionKeyName { get; private set; }
+
+    public string? MacKeyName { get; private set; }
+
+    public ReferenceExpression ConnectionStringExpression => Url;
+
+    public IEnumerable<KeyValuePair<string, ReferenceExpression>> GetConnectionProperties()
+    {
+        yield return new("Url", this.Url);
+
+        if (this.AuthKeyId is not null)
+        {
+            yield return new("AuthKeyId", ReferenceExpression.Create($"{this.AuthKeyId}"));
+        }
+        if (this.Password is not null)
+        {
+            yield return new("Password", ReferenceExpression.Create($"{this.Password}"));
+        }
+        if (this.EncryptionKey is not null)
+        {
+            yield return new("EncryptionKey", ReferenceExpression.Create($"{this.EncryptionKey}"));
+        }
+        if (this.MacKey is not null)
+        {
+            yield return new("MacKey", ReferenceExpression.Create($"{this.MacKey}"));
+        }
+        if (this.EncryptionKeyName is not null)
+        {
+            yield return new("EncryptionKeyName", ReferenceExpression.Create($"{this.EncryptionKeyName}"));
+        }
+        if (this.MacKeyName is not null)
+        {
+            yield return new("MacKeyName", ReferenceExpression.Create($"{this.MacKeyName}"));
+        }
+    }
+
+    internal void SetPassword(ParameterResource authKeyId, ParameterResource password)
+    {
+        this.AuthKeyId = authKeyId;
+        this.Password = password;
+        this.EncryptionKey = null;
+        this.MacKey = null;
+        this.EncryptionKeyName = null;
+        this.MacKeyName = null;
+    }
+
+    internal void SetKeys(ParameterResource authKeyId, ParameterResource encryptionKey, ParameterResource macKey)
+    {
+        this.AuthKeyId = authKeyId;
+        this.Password = null;
+        this.EncryptionKey = encryptionKey;
+        this.MacKey = macKey;
+        this.EncryptionKeyName = null;
+        this.MacKeyName = null;
+    }
+
+    internal void SetKeyNames(ParameterResource authKeyId, string encryptionKeyName, string macKeyName)
+    {
+        this.AuthKeyId = authKeyId;
+        this.Password = null;
+        this.EncryptionKey = null;
+        this.MacKey = null;
+        this.EncryptionKeyName = encryptionKeyName;
+        this.MacKeyName = macKeyName;
+    }
 }
--- YubiHsmSharp.Hosting/YubiHsmResourceBuilderExtensions.cs
+++ YubiHsmSharp.Hosting/YubiHsmResourceBuilderExtensions.cs
@@ -6,13 +6,46 @@ public static class YubiHsmResourceBuilderExtensions
 {
     extension(IDistributedApplicationBuilder builder)
     {
-        public IResourceBuilder<YubiHsmResource> AddYubiHsm(
-            [ResourceName] string name,
-            string? yubiHsmUrl = null)
+        public IResourceBuilder<YubiHsmResource> AddYubiHsm([ResourceName] string name)
         {
             YubiHsmResource resource = new(name);
-            return builder.AddResource(resource)
-                .WithEndpoint();
+            return builder.AddResource(resource);
+        }
+    }
+
+    extension(IResourceBuilder<YubiHsmResource> builder)
+    {
+        public IResourceBuilder<YubiHsmResource> WithHttpEndpoint(string targetHost = "localhost", int targetPort = 12345)
+        {
+            return builder.WithEndpoint(YubiHsmResource.YubiHsmEndpointName, e =>
+            {
+                e.TargetHost = targetHost;
+                e.TargetPort = targetPort;
+                e.UriScheme = "http";
+            });
+        }
+
+        public IResourceBuilder<YubiHsmResource> WithUsbEndpoint()
+        {
+            return builder.WithEndpoint(name: YubiHsmResource.YubiHsmEndpointName, scheme: "yhusb");
+        }
+
+        public IResourceBuilder<YubiHsmResource> WithPassword(IResourceBuilder<ParameterResource> authKeyId, IResourceBuilder<ParameterResource> password)
+        {
+            builder.Resource.SetPassword(authKeyId.Resource, password.Resource);
+            return builder;
+        }
+
+        public IResourceBuilder<YubiHsmResource> WithKeys(IResourceBuilder<ParameterResource> authKeyId, IResourceBuilder<ParameterResource> encryptionKey, IResourceBuilder<ParameterResource> macKey)
+        {
+            builder.Resource.SetKeys(authKeyId.Resource, encryptionKey.Resource, macKey.Resource);
+            return builder;
+        }
+
+        public IResourceBuilder<YubiHsmResource> WithNamedKeys(IResourceBuilder<ParameterResource> authKeyId, string encryptionKeyName, string macKeyName)
+        {
+            builder.Resource.SetKeyNames(authKeyId.Resource, encryptionKeyName, macKeyName);
+            return builder;
         }
     }
 }
