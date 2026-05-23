c0af44bbf752019201c29e9edd6a89925c8f687b
Ben Callaghan <benj.callaghan@gmail.com>
2026-05-22 21:41:11 -0600

    found a very hacky way to attach extra parameters to external service

--- YubiHsmSharp.AppHost/AppHost.cs
+++ YubiHsmSharp.AppHost/AppHost.cs
@@ -1,13 +1,64 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var yubiAuthKey = builder.AddParameter("YubiHSM-AuthKeyId");
-var yubiPassword = builder.AddParameter("YubiHSM-Password", secret: true);
+// var yubihsmExt = builder.AddExternalService("yubihsm", "http://localhost:12345");
+// var yubihsm = builder.AddResource(new YubiHsmResource(yubihsmExt));
 
-var yubiHsm = builder.AddYubiHsm("yubihsm")
-    .WithHttpEndpoint("localhost")
-    .WithPassword(yubiAuthKey, yubiPassword);
+//var yubihsm = builder.AddYubiHsm("yubihsm");
+// var yubihsm = builder.AddResource(new YubiHsmResource("yubihsm"))
+//     .WithEndpoint("http", endpoint =>
+//     {
+//         endpoint.TargetPort = 12345;
+//     });
 
-builder.AddProject<Projects.YubiHsmSharp_Demo>("demo")
-    .WithReference(yubiHsm);
+var authKeyId = builder.AddParameter("YubiHsm-AuthKeyId");
+var password = builder.AddParameter("YubiHsm-Password");
+
+var yubihsm = AddYubiHsm(builder, "yubihsm", "http://localhost:12345");
+WithPassword(yubihsm, authKeyId, password);
+
+var demo = builder.AddProject<Projects.YubiHsmSharp_Demo>("demo");
+WithReference(demo, yubihsm);
 
 builder.Build().Run();
+
+IResourceBuilder<YubiHsmResource> AddYubiHsm(IDistributedApplicationBuilder builder, string name, string url)
+{
+    var external = builder.AddExternalService(name, url);
+    return new YubiHsmResourceBuilder(external);
+}
+
+IResourceBuilder<YubiHsmResource> WithPassword(IResourceBuilder<YubiHsmResource> builder, IResourceBuilder<ParameterResource> authKeyId, IResourceBuilder<ParameterResource> password)
+{
+    builder.Resource.AuthKeyId = authKeyId.Resource;
+    builder.Resource.Password = password.Resource;
+    return builder;
+}
+
+IResourceBuilder<TDestination> WithReference<TDestination>(IResourceBuilder<TDestination> builder, IResourceBuilder<YubiHsmResource> yubihsm) where TDestination : IResourceWithEnvironment
+{
+    return builder.WithReference(yubihsm.Resource.External)
+        .WithEnvironment("YubiHsm__AuthKeyId", yubihsm.Resource.AuthKeyId)
+        .WithEnvironment("YubiHsm__Password", yubihsm.Resource.Password);
+}
+
+internal class YubiHsmResource(IResourceBuilder<ExternalServiceResource> external) : Resource(external.Resource.Name)
+{
+    public IResourceBuilder<ExternalServiceResource> External => external;
+
+    public ParameterResource AuthKeyId { get; internal set; }
+
+    public ParameterResource Password { get; internal set; }
+}
+
+internal class YubiHsmResourceBuilder(IResourceBuilder<ExternalServiceResource> external) : IResourceBuilder<YubiHsmResource>
+{
+    public IDistributedApplicationBuilder ApplicationBuilder => external.ApplicationBuilder;
+
+    public YubiHsmResource Resource { get; } = new YubiHsmResource(external);
+
+    public IResourceBuilder<YubiHsmResource> WithAnnotation<TAnnotation>(TAnnotation annotation, ResourceAnnotationMutationBehavior behavior = ResourceAnnotationMutationBehavior.Append) where TAnnotation : IResourceAnnotation
+    {
+        var ext = external.WithAnnotation(annotation, behavior);
+        return new YubiHsmResourceBuilder(ext);
+    }
+}
