6c543a3f75c2a20a5f6e272e0d35db013087ec58
Ben Callaghan <benj.callaghan@gmail.com>
2026-05-25 23:23:42 -0600

    added buffering settings for the write file

--- YubiHsmSharp/DebugFile.cs
+++ YubiHsmSharp/DebugFile.cs
@@ -5,8 +5,10 @@ namespace YubiHsmSharp;
 
 internal partial class DebugFile : IDisposable
 {
-    private const int O_BINARY = 0x8000;
-    private const int O_NOINHERIT = 0x0080;
+    private const int WIN_BINARY = 0x8000;
+    private const int WIN_NOINHERIT = 0x0080;
+    private const int WIN_IOLBF = 0x0040;
+    private const int POSIX_IOLBF = 0x0001;
 
     [LibraryImport("ucrtbase.dll", EntryPoint = "_pipe", SetLastError = true)]
     private static partial int win_pipe(Span<int> pfds, uint psize, int textmode);
@@ -14,11 +16,17 @@ internal partial class DebugFile : IDisposable
     [LibraryImport("ucrtbase.dll", EntryPoint = "_fdopen", SetLastError = true)]
     private static partial nint win_fdopen(int fd, ReadOnlySpan<byte> mode);
 
+    [LibraryImport("ucrtbase.dll", EntryPoint = "setvbuf", SetLastError = true)]
+    private static partial int win_setvbuf(nint stream, nint buffer, int mode, nuint size);
+
     [LibraryImport("libc", EntryPoint = "pipe", SetLastError = true)]
     private static partial int posix_pipe(Span<int> pipefd);
 
     [LibraryImport("libc", EntryPoint = "fdopen", SetLastError = true)]
     private static partial nint posix_fdopen(int filedes, ReadOnlySpan<byte> mode);
+
+    [LibraryImport("libc", EntryPoint = "setvbuf", SetLastError = true)]
+    private static partial int posix_setvbuf(nint stream, nint buf, int type, nuint size);
 
     private readonly SafeFileHandle readHandle;
     private readonly SafeFileHandle writeHandle;
@@ -28,10 +36,11 @@ internal partial class DebugFile : IDisposable
     {
         // FIXME: In .NET 11, much of this can be replaced with SafeFileHandle.CreateAnonymousPipe()
         Span<int> fds = stackalloc int[2];
+        const int bufferSize = 4096;
 
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
-            int rc = win_pipe(fds, 4096, O_BINARY | O_NOINHERIT);
+            int rc = win_pipe(fds, bufferSize, WIN_BINARY | WIN_NOINHERIT);
             ThrowIfError(rc);
 
             this.readHandle = new(fds[0], ownsHandle: true);
@@ -39,6 +48,9 @@ internal partial class DebugFile : IDisposable
 
             this.writeFile = win_fdopen((int)this.writeHandle.DangerousGetHandle(), "w"u8);
             ThrowIfNull(this.writeFile);
+
+            rc = win_setvbuf(this.writeFile, 0, WIN_IOLBF, bufferSize);
+            ThrowIfError(rc);
         }
         else
         {
@@ -50,6 +62,9 @@ internal partial class DebugFile : IDisposable
 
             this.writeFile = posix_fdopen((int)this.writeHandle.DangerousGetHandle(), "w"u8);
             ThrowIfNull(this.writeFile);
+
+            rc = posix_setvbuf(this.writeFile, 0, POSIX_IOLBF, bufferSize);
+            ThrowIfError(rc);
         }
     }
 
