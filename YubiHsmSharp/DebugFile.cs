3d541934525e3da5628a1036c1c5622e6002a228
Ben Callaghan <benj.callaghan@gmail.com>
2026-05-25 23:08:02 -0600

    added conversion of file descriptor to FILE*

--- YubiHsmSharp/DebugFile.cs
+++ YubiHsmSharp/DebugFile.cs
@@ -11,8 +11,14 @@ internal partial class DebugFile
     [LibraryImport("ucrtbase.dll", EntryPoint = "_pipe", SetLastError = true)]
     private static partial int win_pipe(Span<int> pfds, uint psize, int textmode);
 
+    [LibraryImport("ucrtbase.dll", EntryPoint = "_fdopen", SetLastError = true)]
+    private static partial nint win_fdopen(int fd, ReadOnlySpan<byte> mode);
+
     [LibraryImport("libc", EntryPoint = "pipe", SetLastError = true)]
     private static partial int posix_pipe(Span<int> pipefd);
+
+    [LibraryImport("libc", EntryPoint = "fdopen", SetLastError = true)]
+    private static partial nint posix_fdopen(int filedes, ReadOnlySpan<byte> mode);
 
     public static DebugFile Create()
     {
@@ -23,20 +29,38 @@ internal partial class DebugFile
         {
             int rc = win_pipe(fds, 4096, O_BINARY | O_NOINHERIT);
             ThrowIfError(rc);
+
+            SafeFileHandle readFD = new(fds[0], ownsHandle: true);
+            SafeFileHandle writeFD = new(fds[1], ownsHandle: true);
+
+            nint file = win_fdopen((int)writeFD.DangerousGetHandle(), "w"u8);
+            ThrowIfNull(file);
         }
         else
         {
             int rc = posix_pipe(fds);
             ThrowIfError(rc);
-        }
 
-        SafeFileHandle readFD = new(fds[0], ownsHandle: true);
-        SafeFileHandle writeFD = new(fds[1], ownsHandle: true);
+            SafeFileHandle readFD = new(fds[0], ownsHandle: true);
+            SafeFileHandle writeFD = new(fds[1], ownsHandle: true);
+
+            nint file = posix_fdopen((int)writeFD.DangerousGetHandle(), "w"u8);
+            ThrowIfNull(file);
+        }
     }
 
     private static void ThrowIfError(int rc)
     {
         if (rc != 0)
+        {
+            // Win32Exception properly handles errno/GetLastPInvokeError, even on non-Windows systems.
+            throw new Win32Exception();
+        }
+    }
+
+    private static void ThrowIfNull(nint ptr)
+    {
+        if (ptr == 0)
         {
             // Win32Exception properly handles errno/GetLastPInvokeError, even on non-Windows systems.
             throw new Win32Exception();
