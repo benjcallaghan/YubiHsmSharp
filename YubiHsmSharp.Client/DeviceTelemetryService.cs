461ffc833d3e449dedcb676aa9402ab4e2f467d3
Ben Callaghan <benj.callaghan@gmail.com>
2026-05-25 20:43:59 -0600

    added device log counts

--- YubiHsmSharp.Client/DeviceTelemetryService.cs
+++ YubiHsmSharp.Client/DeviceTelemetryService.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Diagnostics.Metrics;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,17 @@ internal class DeviceTelemetryService(IServiceScopeFactory scopeFactory, string?
 {
     internal const string MeterName = "YubiHsmSharp.Client";
     private static readonly Meter Meter = new(MeterName);
+
+    private static readonly Gauge<byte> LogTotal = Meter.CreateGauge<byte>(
+        name: "yubihsm.log.available",
+        unit: "{logs}",
+        description: "The current number of available logs stored in the device"
+    );
+    private static readonly Gauge<byte> LogUsed = Meter.CreateGauge<byte>(
+        name: "yubihsm.log.used",
+        unit: "{logs}",
+        description: "The current number of used logs stored in the device"
+    );
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
@@ -22,6 +34,13 @@ internal class DeviceTelemetryService(IServiceScopeFactory scopeFactory, string?
                 : scope.ServiceProvider.GetRequiredKeyedService<YubiConnector>(serviceKey);
 
             var device = connector.GetDeviceInfo();
+            TagList deviceTags = [
+                new("yubihsm.version", $"{device.Major}.{device.Minor}.{device.Patch}"),
+                new("yubihsm.serial", device.Serial),
+            ];
+
+            LogTotal.Record(device.LogTotal, in deviceTags);
+            LogUsed.Record(device.LogUsed, in deviceTags);
         } while (await timer.WaitForNextTickAsync(stoppingToken));
     }
 }
