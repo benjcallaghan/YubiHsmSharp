533bc87c823c4d5c714cb712a5eec12d50fc0474
Ben Callaghan <benj.callaghan@gmail.com>
2026-06-01 21:12:31 -0600

    added shim for signing with RSA-PSS

--- YubiHsmSharp.BouncyCastle/README.md
+++ YubiHsmSharp.BouncyCastle/README.md
@@ -5,8 +5,12 @@ A compatibility shim that exposes YubiHSM 2 operations through BouncyCastle-comp
 ## Supported Types
 
 * `YubiAesBlockCipher` implements `IBlockCipher`
-* `YubiAsymmetricKeyParameter` implements `KeyParameter`
 * `YubiRandomGenerator` implements `IRandomGenerator`
 * `YubiRsaKeyParameters` implements `RsaKeyParameters`
-* `YubiRsaPkcsBlockCipher` implements `IAsyncBlockCipher`
+* `YubiRsaPkcsBlockCipher` implements `IAsymmetricBlockCipher`
+* `YubiRsaPssBlockCipher` implements `IAsymmetricBlockCipher`
 * `YubiSymmetricKeyParameter` implements `KeyParameter`
+
+### Key Parameters
+
+The provided implementations of `ICipherParameters` only work with the ciphers and implementations provided by this library. The key parameters contain only the identifier of the key within a YubiHSM 2 device. Thus, there is no key data to use with standard BouncyCastle-implemented ciphers.
---
+++ YubiHsmSharp.BouncyCastle/YubiRsaPssBlockCipher.cs
@@ -0,0 +1,64 @@
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Security;
+
+namespace YubiHsmSharp.BouncyCastle;
+
+/// <summary>
+/// An RSA block cipher that uses the RSA-PSS mode of the YubiHSM 2.
+/// </summary>
+/// <remarks>
+/// The authentication key used to create the session must have the following capabilities:
+/// sign-pss.
+/// </remarks>
+/// <param name="session">The authenticated session with the YubiHSM 2.</param>
+/// <param name="maskGenerationFunction">The mask generation function to use.</param>
+public class YubiRsaPssBlockCipher(YubiSession session, Algorithm maskGenerationFunction)
+: IAsymmetricBlockCipher
+{
+    private ushort keyId;
+    private int modulusLength;
+
+    /// <inheritdoc />
+    public string AlgorithmName => "RSS-PSS";
+
+    /// <inheritdoc />
+    public void Init(bool forEncryption, ICipherParameters parameters)
+    {
+        if (!forEncryption)
+        {
+            throw new ArgumentException("YubiRsaPssBlockCipher only supports signing (encryption) operations.", nameof(forEncryption));
+        }
+
+        if (parameters is YubiRsaKeyParameters rsaParameters)
+        {
+            if (!rsaParameters.IsPrivate)
+            {
+                throw new InvalidKeyException("This cipher only supports private key operations. For public key operations, use the standard BouncyCastle RSA engine.");
+            }
+
+            this.keyId = rsaParameters.KeyId;
+            this.modulusLength = rsaParameters.Modulus.BitLength / 8;
+        }
+    }
+
+    /// <inheritdoc />
+    public int GetInputBlockSize() => maskGenerationFunction switch
+    {
+        Algorithm.Mgf1Sha1 => this.modulusLength - 2 * 20 - 2,
+        Algorithm.Mgf1Sha256 => this.modulusLength - 2 * 32 - 2,
+        Algorithm.Mgf1Sha384 => this.modulusLength - 2 * 48 - 2,
+        Algorithm.Mgf1Sha512 => this.modulusLength - 2 * 64 - 2,
+        _ => throw new NotSupportedException("Unsupported mask generation function. Supported functions are MGF1 with SHA-1, SHA-256, SHA-384, and SHA-512."),
+    };
+
+    /// <inheritdoc />
+    public int GetOutputBlockSize() => this.modulusLength;
+
+    /// <inheritdoc />
+    public byte[] ProcessBlock(byte[] inBuf, int inOff, int inLen)
+    {
+        Span<byte> output = stackalloc byte[this.GetOutputBlockSize()];
+        int written = session.SignPss(this.keyId, inBuf.AsSpan(inOff, inLen), output, inLen, maskGenerationFunction);
+        return output[..written].ToArray();
+    }
+}
