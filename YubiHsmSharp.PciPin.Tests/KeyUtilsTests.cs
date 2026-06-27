62005364fdbe72f575a52d6008cc2b8ac0d09c32
Benjamin Callaghan <benj.callaghan@gmail.com>
2026-06-26 22:13:29 -0600

    added test for key components

--- YubiHsmSharp.PciPin.Tests/KeyUtilsTests.cs
+++ YubiHsmSharp.PciPin.Tests/KeyUtilsTests.cs
@@ -17,7 +17,7 @@
 namespace YubiHsmSharp.PciPin.Tests;
 
 [Trait("Requires", "None")]
-public class UnitTest1
+public class KeyUtilsTests
 {
     [Theory]
     [InlineData("1234567890ABCDEFFEDCBA0987654321", "3F077B")]
@@ -37,5 +37,27 @@ public class UnitTest1
         // Assert
         byte[] checkValue = Convert.FromHexString(hexCheckValue);
         Assert.Equal(checkValue, keyCheckValue);
+    }
+
+    [Fact]
+    public void CombineComponents_WithValidComponents_ProducesKey()
+    {
+        // Arrange
+        ReadOnlySpan<byte> kc1 = [0x98, 0x8A, 0x59, 0xD7, 0x27, 0x31, 0x86, 0xB8,
+                                  0xC9, 0xC9, 0x92, 0x2B, 0x6D, 0x40, 0xBA, 0x75];
+        ReadOnlySpan<byte> kc2 = [0x89, 0x36, 0xE5, 0x26, 0x9A, 0xDF, 0xAB, 0xE7,
+                                  0xD4, 0x82, 0x9B, 0x2E, 0xFB, 0x3B, 0xF5, 0xD9];
+        ReadOnlySpan<byte> kc3 = [0x10, 0x9F, 0xF9, 0x96, 0x34, 0x45, 0xE0, 0xB0,
+                                  0xE3, 0x97, 0xB3, 0x9D, 0xE0, 0x2F, 0x7D, 0xBC];
+        ReadOnlySpan<byte> key = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF,
+                                  0xFE, 0xDC, 0xBA, 0x98, 0x76, 0x54, 0x32, 0x10];
+
+        // Act
+        Span<byte> buffer = stackalloc byte[16];
+        int written = KeyUtils.CombineComponents(kc1, kc2, kc3, buffer);
+        buffer = buffer[..written];
+
+        // Assert
+        Assert.Equal(key, buffer);
     }
 }
